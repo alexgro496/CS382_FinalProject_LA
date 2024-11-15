6c62a66f00b3d81ee7cca351f81b430a8278b15b
alexgro496 <sewcool0504@yahoo.com>
2024-11-15 13:02:36 -0600

    update float to fix bug

--- Assets/__Scripts/Scene Transitions/Scene_3_Transitions/Scene_3Eto3D_Transition.cs
+++ Assets/__Scripts/Scene Transitions/Scene_3_Transitions/Scene_3Eto3D_Transition.cs
@@ -11,7 +11,7 @@ public class Scene_3Eto3D_Transition : MonoBehaviour
         
         Vector3 pos = transform.position;
         pos.x = (float).5;
-        pos.y = (float)5;
+        pos.y = (float)4;
         MainCharacter.S.transform.position = pos;
         
         SceneManager.LoadScene("__Scene_3d_Maze_TL");
